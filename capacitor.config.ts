2e670f9f94b1259c8c41ebeb1b2f9508b63626c7
Christian Bromann <git@bromann.dev>
2023-12-14 19:08:47 -0800

    Rename some metadata

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.webdriver.demo',
   appName: 'WebdriverIO Demo Application',
   webDir: 'dist',
   server: {
